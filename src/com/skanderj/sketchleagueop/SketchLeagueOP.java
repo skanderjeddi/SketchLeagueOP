86bcd51a99ee84ef3f7017a438e1367bb8ed8180
skander-j <skander.jeddi2@gmail.com>
2019-04-04 23:54:05 +0200

    First commit

---
+++ src/com/skanderj/sketchleagueop/SketchLeagueOP.java
@@ -0,0 +1,72 @@
+package com.skanderj.sketchleagueop;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+public final class SketchLeagueOP {
+	public static final String[] CHAMPIONS = { "Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Aurelion Sol", "Azir", //
+			"Bard", "Blitzcrank", "Brand", "Braum", //
+			"Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki", //
+			"Darius", "Diana", "Dr. Mundo", "Draven", //
+			"Ekko", "Elise", "Evelynn", "Ezreal", //
+			"Fiddlesticks", "Fiora", "Fizz", //
+			"Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", //
+			"Hecarim", "Heimerdinger", //
+			"Illaoi", "Irelia", "Ivern", //
+			"Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", //
+			"Kai'Sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", //
+			"LeBlanc", "Lee Sin", "Leona", "Lissandra", "Lucian", "Lulu", "Lux", //
+			"Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", //
+			"Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nocturne", "Nunu & Willump", //
+			"Olaf", "Orianna", "Ornn", //
+			"Pantheon", "Poppy", "Pyke", //
+			"Quinn", //
+			"Rakan", "Rammus", "Rek'Sai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", //
+			"Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sion", "Soraka", "Swain", "Sylas", "Syndra", //
+			"Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", //
+			"Udyr", "Urgot", //
+			"Varus", "Vayne", "Veigar", "Vel'Koz", "Vi", "Viktor", "Vladimir", "Volibear", //
+			"Warwick", "Wukong", //
+			"Xayah", "Xerath", "Xin Zhao", //
+			"Yasuo", "Yorick", //
+			"Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra" };
+
+	private Robot robot;
+
+	public SketchLeagueOP() throws AWTException {
+		this.robot = new Robot();
+	}
+
+	public final void tryAllChampions() {
+		this.robot.delay(4000);
+		for (String champion : SketchLeagueOP.CHAMPIONS) {
+			this.type(champion);
+			this.type(KeyEvent.VK_ENTER);
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			new SketchLeagueOP().tryAllChampions();
+		} catch (AWTException awtException) {
+			awtException.printStackTrace();
+		}
+	}
+
+	private final void type(int code) {
+		this.robot.delay(40);
+		this.robot.keyPress(code);
+		this.robot.keyRelease(code);
+	}
+
+	private final void type(String string) {
+		byte[] bytes = string.getBytes();
+		for (byte currentByte : bytes) {
+			int code = currentByte;
+			this.robot.delay(40);
+			this.robot.keyPress(code);
+			this.robot.keyRelease(code);
+		}
+	}
+}
